45f920828a2b578138f7bb957ef228f354da8a5d
Mateusz Matejuk (esavier) <esavier@erglabs.org>
2021-08-04 22:12:45 +0200

    - yanked the code to separate module - wrote some docs

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 #![allow(unused_imports)]
 
+pub mod probe;
+use probe::*;
+
 use async_trait::async_trait;
 use futures::{channel::mpsc, executor::LocalPool, prelude::*, task::SpawnExt, AsyncWriteExt};
 use rand::{self, Rng};
-use std::{io, iter};
-use std::{collections::HashSet, num::NonZeroU16};
+use std::io;
+use std::iter;
 use libp2p::{
     core::{
         Multiaddr,
@@ -12,36 +15,35 @@ use libp2p::{
         identity,
         muxing::StreamMuxerBox,
         transport::{self, Transport},
-        upgrade::{self, read_length_prefixed, write_length_prefixed}
+        upgrade,
     },
     yamux::{YamuxConfig,self},
     tcp::{TcpConfig,self},
     swarm::{Swarm, SwarmEvent},
     noise::{NoiseConfig, X25519Spec, Keypair},
-    request_response::*,
 };
 
 fn main() {
-    let query = ScaffoldQuery("query".to_string().into_bytes());
-    let response = ScaffoldResponse("response".to_string().into_bytes());
+    let query = ProbeQuery("query".to_string().into_bytes());
+    let response = ProbeResponse("response".to_string().into_bytes());
 
     // very important to make protocols an iterable list
     // and make sure that the protocols are bound to codecs
     // and direction hints
-    let protocols = iter::once((ScaffoldProtocol(), ProtocolSupport::Full));
+    let protocols = iter::once((ProbeProtocol(), ProtocolSupport::Full));
     let cfg = RequestResponseConfig::default();
 
     // magicaly bind peer to channel
     // basically creates a peer with stream bound to it
     // actual transprot does not matter
-    // this is what client/server should do to use scaffold
+    // this is what client/server should do to use Probe
     let (peer1_id, trans) = mk_transport();
-    let queryprotoA = RequestResponse::new(ScaffoldCodec(), protocols.clone(), cfg.clone());
+    let queryprotoA = RequestResponse::new(ProbeCodec(), protocols.clone(), cfg.clone());
     let mut swarm1 = Swarm::new(trans, queryprotoA, peer1_id);
     
     // same as above
     let (peer2_id, trans) = mk_transport();
-    let queryprotoB = RequestResponse::new(ScaffoldCodec(), protocols, cfg);
+    let queryprotoB = RequestResponse::new(ProbeCodec(), protocols, cfg);
     let mut swarm2 = Swarm::new(trans, queryprotoB, peer2_id);
 
     // bind comm channel between peers to run in background,
@@ -79,7 +81,7 @@ fn main() {
         }
     };
 
-    let num_pings: u8 = rand::thread_rng().gen_range(1, 100);
+    let num_pings: u8 = 5;
 
     let peer2 = async move {
         let mut count = 0;
@@ -126,81 +128,4 @@ fn mk_transport() -> (PeerId, transport::Boxed<(PeerId, StreamMuxerBox)>) {
         .authenticate(NoiseConfig::xx(noise_keys).into_authenticated())
         .multiplex(libp2p::yamux::YamuxConfig::default())
         .boxed())
-}
-
-// Simple Ping-Pong Protocol
-
-#[derive(Debug, Clone)]
-struct ScaffoldProtocol();
-
-#[derive(Clone)]
-struct ScaffoldCodec();
-
-#[derive(Debug, Clone, PartialEq, Eq)]
-struct ScaffoldQuery(Vec<u8>);
-
-#[derive(Debug, Clone, PartialEq, Eq)]
-struct ScaffoldResponse(Vec<u8>);
-
-impl ProtocolName for ScaffoldProtocol {
-    fn protocol_name(&self) -> &[u8] {
-        "/scaffold/1".as_bytes()
-    }
-}
-
-#[async_trait]
-impl RequestResponseCodec for ScaffoldCodec {
-    type Protocol = ScaffoldProtocol;
-    type Request = ScaffoldQuery;
-    type Response = ScaffoldResponse;
-
-    async fn read_request<T>(&mut self, _: &ScaffoldProtocol, io: &mut T)
-        -> io::Result<Self::Request>
-    where
-        T: AsyncRead + Unpin + Send
-    {
-        let vec = read_length_prefixed(io, 1024).await?;
-
-        if vec.is_empty() {
-            return Err(io::ErrorKind::UnexpectedEof.into())
-        }
-
-        Ok(ScaffoldQuery(vec))
-    }
-
-    async fn read_response<T>(&mut self, _: &ScaffoldProtocol, io: &mut T)
-        -> io::Result<Self::Response>
-    where
-        T: AsyncRead + Unpin + Send
-    {
-        let vec = read_length_prefixed(io, 1024).await?;
-
-        if vec.is_empty() {
-            return Err(io::ErrorKind::UnexpectedEof.into())
-        }
-
-        Ok(ScaffoldResponse(vec))
-    }
-
-    async fn write_request<T>(&mut self, _: &ScaffoldProtocol, io: &mut T, ScaffoldQuery(data): ScaffoldQuery)
-        -> io::Result<()>
-    where
-        T: AsyncWrite + Unpin + Send
-    {
-        write_length_prefixed(io, data).await?;
-        io.close().await?;
-
-        Ok(())
-    }
-
-    async fn write_response<T>(&mut self, _: &ScaffoldProtocol, io: &mut T, ScaffoldResponse(data): ScaffoldResponse)
-        -> io::Result<()>
-    where
-        T: AsyncWrite + Unpin + Send
-    {
-        write_length_prefixed(io, data).await?;
-        io.close().await?;
-
-        Ok(())
-    }
 }
